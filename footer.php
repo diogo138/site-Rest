8b7e173f6ed0ef63e19ba734930bff5046829b7c
Diogo henrique <diogomike138@gmail.com>
2021-12-10 05:32:43 -0300

    wip: Adicionado funcao menu no wp.

---
+++ footer.php
@@ -0,0 +1,19 @@
+
+
+		
+
+		<footer>
+			<nav class="nav-footer">
+				<ul>
+				<li><a href="/developer/">Menu</a></li>
+					<li class="current_page_item"><a href="/developer/sobre/">Sobre</a></li>
+					<li><a href="/developer/contato/">Contato</a></li>
+				</ul>
+			</nav>
+
+			<p>Rest © <?php echo date("Y"); ?>. Alguns direitos reservados.</p>
+			
+		</footer>
+		<?php wp_footer(); ?>
+	</body>
+</html>
